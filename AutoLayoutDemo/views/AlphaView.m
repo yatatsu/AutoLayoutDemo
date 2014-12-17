9f89369a2d4854531e83601c1f2a25511906cf98
kitagawa <kitagawa@appirits.com>
2014-12-17 20:32:37 +0900

    add AlphaView

---
+++ AutoLayoutDemo/views/AlphaView.m
@@ -0,0 +1,13 @@
+//
+//  AlphaView.m
+//  AutoLayoutDemo
+//
+//  Created by kitagawa on 2014/12/17.
+//  Copyright (c) 2014年 kitagawa. All rights reserved.
+//
+
+#import "AlphaView.h"
+
+@implementation AlphaView
+
+@end
